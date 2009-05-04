6b3d40c8a62333ff71357901e50f15fb645f9941
Ziling Zhao <zilingzhao@gmail.com>
2009-05-03 17:25:38 -0700

    simply display the plasmoid name

---
+++ contents/code/main.py
@@ -0,0 +1,28 @@
+#
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyKDE4.plasma import Plasma
+from PyKDE4 import plasmascript
+
+class DoThisNow(plasmascript.Applet):
+    def __init__(self, parent, args=None):
+        plasmascript.Applet.__init__(self, parent)
+
+    def init(self):
+        self.setHasConfigurationInterface(False)
+        self.setAspectRatioMode(Plasma.Square)
+
+        self.theme = Plasma.Svg(self)
+        self.theme.setImagePath("widgets/background")
+        self.setBackgroundHints(Plasma.Applet.DefaultBackground)
+
+        self.layout = QGraphicsLinearLayout(Qt.Horizontal, self.applet)
+        label = Plasma.Label(self.applet)
+        label.setText("DoThisNow")
+        self.layout.addItem(label)
+        self.setLayout(self.layout)
+        self.resize(125,125)
+
+def CreateApplet(parent):
+    return DoThisNow(parent)
