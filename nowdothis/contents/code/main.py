bdabe6b79e8153f584df57b81265b8ff6080e07e
Ziling Zhao <zilingzhao@gmail.com>
2009-05-07 00:27:56 -0700

    first passably usable version, allows for editing and finishing of tasks

--- nowdothis/contents/code/main.py
+++ nowdothis/contents/code/main.py
@@ -20,19 +20,43 @@ class NowDoThisMoid(plasmascript.Applet):
         self.theme.setImagePath("widgets/background")
         self.setBackgroundHints(Plasma.Applet.DefaultBackground)
 
-        self.layout = QGraphicsLinearLayout(Qt.Horizontal, self.applet)
-        label = Plasma.Label(self.applet)
-        label.setText("DoThisNow")
-        self.layout.addItem(label)
-        self.setLayout(self.layout)
-        self.resize(125,125)
-
         self.settings = {}
         gc = self.config()
-
         self.settings["editor"] = gc.readEntry("editor", "/usr/bin/kwrite")
-
         self.ndt = NowDoThis(self.settings["editor"])
+
+        self.layout = QGraphicsLinearLayout(Qt.Vertical, self.applet)
+        self.setLayout(self.layout)
+
+        self.task = Plasma.Label(self.applet)
+        self.task.setText(self.ndt.curTask() or "No Tasks")
+        self.layout.addItem(self.task)
+
+        done = Plasma.PushButton(self.applet)
+        done.setText("Done")
+        self.layout.addItem(done)
+        self.connect(done, SIGNAL("clicked()"), self.finishTask)
+
+        edit = Plasma.PushButton(self.applet)
+        edit.setText("Edit")
+        self.layout.addItem(edit)
+        self.connect(edit, SIGNAL("clicked()"), self.edit)
+
+        self.resize(125,125)
+
+
+    def updateTaskText(self):
+        self.task.setText(self.ndt.curTask() or "No Tasks")
+        self.task.update()
+
+    def edit(self):
+        self.ndt.edit()
+        self.updateTaskText()
+
+    def finishTask(self):
+        if self.ndt.finishTask():
+            self.ndt.save()
+            self.updateTaskText()
 
 
 def CreateApplet(parent):
